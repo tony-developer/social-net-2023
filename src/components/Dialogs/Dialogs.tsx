047fef9f4219f40b912fa5ecae0026ca9dee0ebd
tony-developer <anton.serebriakoff@gmail.com>
2023-03-02 18:12:06 -0800

    commit-19 + Routing

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from "./Dialog.module.css"
+
+const Dialogs = (props:any) => {
+    return (
+        <div>
+            Dialogs
+        </div>
+    );
+};
+
+export default Dialogs;
